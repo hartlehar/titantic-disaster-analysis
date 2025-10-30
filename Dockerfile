350c10ad29b1f68225ac1935ab1f4c026dfe40d6
Hartley <tranhartley@gmail.com>
2025-10-30 17:54:19 -0500

    finalize python script

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 WORKDIR /src
 
-CMD ["python3", "app.py"]
+CMD ["python3", "train.py"]
--- src/train.py
+++ src/train.py
@@ -9,8 +9,17 @@ train = pd.read_csv("data/train.csv")
 train = train.dropna()
 test = pd.read_csv('data/test.csv')
 
+print("---- Data Overview ----")
+print("Train Set:")
+print(train.info())
+print()
+print("Test Set:")
+print(test.info())
+
 
 ### Transform Data
+print("\n\n---- Transforming Data ----")
+
 # Fill NaN values in Age column of the test set to median age 
 # by Pclass and sex from train set
 age_medians = train.groupby(['Pclass', 'Sex'])['Age'].median()
@@ -22,42 +31,54 @@ def fill_age(row):
         return row['Age']
 
 test['Age'] = test.apply(fill_age, axis=1)
+print("The `test` set has several NaN values in the `Age` columns. \
+These NaN values will be replaced with the median age based on the passenger's age and their ticket class.")
 
 # Convert Sex column into integer, 1 = male and 0 = female
 train['Sex'] = (train['Sex'] == 'male').astype(int)
 test['Sex'] = (test['Sex'] == 'male').astype(int)
+print("The `Sex` column is a string which cannot be used in logistic regression. \
+Converting it into a binary variable makes it compatible.")
 
 # Convert Cabin into categorical column
 train['Cabin'] = train['Cabin'].astype('category')
 test['Cabin'] = test['Cabin'].astype('category')
+print("The `Cabin` column is a string which cannot be used in logistic regression. \
+Converting it into a categorical variable.")
 
 # Convert Embarked into categorical column
 train['Embarked'] = train['Embarked'].astype('category')
 test['Embarked'] = test['Embarked'].astype('category')
-
+print("The `Embarked` column is a string which cannot be used in logistic regression. \
+Converting it into a categorical variable.")
 
 ### Train and Evaluate Model
+print("\n\n---- Training and Evaluation ----")
+
 # Initialize model
 model = LogisticRegression()
 k_folds = StratifiedKFold(n_splits = 5, shuffle=True, random_state=123)
+print("Since there is only 183 recordings in the training set, K-folds (specifically 5 folds) validation \
+will be used to measure the model's performance.")
 
 # Select features to be trained on
 train_x = train[['Sex', 'Age', 'SibSp', 'Parch', 'Pclass']]
 train_y = train['Survived']
 test_x = test[['Sex', 'Age', 'SibSp', 'Parch', 'Pclass']]
+print("'Sex', 'Age', 'SibSp', 'Parch', 'Pclass' will be the features used in the model.")
 
 # Evaluate cross validation performance
 scores = cross_val_score(model, train_x, train_y, cv=k_folds, scoring='accuracy')
 
-print("Cross Validation Scores:", scores)
-print("Average CV Score:", scores.mean())
-print("Standard Deviation of CV Scores:", scores.std())
+print("Cross Validation Accuracy Scores:", scores)
+print(f"Average CV Score: {scores.mean():.4f}")
+print(f"Standard Deviation of CV Scores: {scores.std():.4f}")
 
 # Fit on full training data
 model.fit(train_x, train_y)
 pred = model.predict(train_x)
 
-accuracy_score(train_y, pred)
+print(f"Model Accuracy on Entire Training Set: {accuracy_score(train_y, pred):.4f}")
 
 
 ### Make Predictions
@@ -69,4 +90,5 @@ results = pd.DataFrame({
     'Survived': test_pred
 })
 
-results.to_csv
+results.to_csv("test_predictions.csv", index=False)
+print("\nTest set predictions saved in 'test_predictions.csv' with 2 columns: PassengerId, Survived")
